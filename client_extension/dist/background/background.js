c78170876d242d134dc7284e1219a9c174887d8f
Ashade Samson <ashadesamson99@gmail.com>
2023-09-27 19:27:21 +0100

    updated manifest rules

--- client_extension/dist/background/background.js
+++ client_extension/dist/background/background.js
@@ -1,40 +1,60 @@
 "use strict";
 /// <reference types="chrome"/>
-chrome.webRequest.onBeforeRequest.addListener((details) => {
-    // capture and extract the url address
+chrome.runtime.onInstalled.addListener(() => {
+    const conditions = [
+        {
+            id: 1,
+            priority: 1,
+            action: {
+                type: "block"
+            },
+            condition: {
+                urlFilter: "<all_urls>",
+            },
+        },
+    ];
+    chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: [1] }, () => {
+        chrome.declarativeNetRequest.updateDynamicRules({
+            addRules: conditions,
+        });
+    });
+});
+chrome.webNavigation.onBeforeNavigate.addListener((details) => {
     const url = details.url;
-    const data = { url };
-    const serverEndpoint = 'http://127.0.0.1:8000/urlapp/check/';
-    const requestObject = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    };
-    // send request with url to server
-    fetch(serverEndpoint, requestObject)
-        .then(res => {
-        if (!res.ok) {
-            throw new Error('Network connection was not OK');
-        }
-        return res.json();
+    const serverEndpoint = "http://127.0.0.1:8000/urlapp/check/";
+    const urlMsg = { url };
+    fetch(serverEndpoint, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(urlMsg),
     })
-        .then(response => {
-        // if url is malicious, block page load and popup warning
-        if (response.is_malicious) {
-            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-                var _a;
-                if ((_a = tabs[0]) === null || _a === void 0 ? void 0 : _a.id) {
-                    chrome.tabs.sendMessage(tabs[0].id, { action: 'showWarning' });
-                }
+        .then((response) => {
+        if (!response.ok) {
+            throw new Error("Network connection not OK");
+        }
+        return response.json();
+    })
+        .then((result) => {
+        if (result.is_malicious) {
+            const tabId = details.tabId;
+            chrome.scripting.executeScript({
+                target: { tabId },
+                function: Warning,
             });
         }
-        else {
-        }
     })
-        .catch(err => {
+        .catch((err) => {
         console.log(err);
     });
-}, { urls: ["<all_urls>"] }, ["blocking"]);
-// Listen to messages from content scripts if needed.
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 });
+function Warning() {
+    const popUp = document.createElement("div");
+    popUp.id = 'warning';
+    popUp.innerText = 'This URL is suspected to be malicious of a potential XSS vulnerability, Proceed with caution';
+    document.body.appendChild(popUp);
+    setTimeout(() => {
+        popUp.remove();
+    }, 10000);
+}
--- client_extension/src/background/background.ts
+++ client_extension/src/background/background.ts
@@ -1,53 +1,77 @@
 /// <reference types="chrome"/>
 
-chrome.webRequest.onBeforeRequest.addListener(
-    (details) => {
-      
-      // capture and extract the url address
-      const url = details.url;
-      const data = {url}
 
-      const serverEndpoint = 'http://127.0.0.1:8000/urlapp/check/'
-      const requestObject = {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
-      }
 
-      // send request with url to server
-      fetch(serverEndpoint, requestObject)
-          .then(res => {
-            if(!res.ok){
-              throw new Error('Network connection was not OK')
-            }
-
-            return res.json()
-          })
-          .then(response => {
-            // if url is malicious, block page load and popup warning
-            if(response.is_malicious){
-              chrome.tabs.query({ active: true, currentWindow: true}, (tabs) => {
-                if(tabs[0]?.id){
-                  chrome.tabs.sendMessage(tabs[0].id, {action: 'showWarning'});
-                }
-              })
-            } else{
-
-            }
-          })
-          .catch( err => {
-            console.log(err);
-          })
+chrome.runtime.onInstalled.addListener(() => {
+  const conditions = [
+    {
+      id: 1,
+      priority: 1,
+      action: {
+        type: "block"
+      },
+      condition: {
+        urlFilter: "<all_urls>",
+      },
     },
-    { urls: ["<all_urls>"] },
-    ["blocking"]
-  );
+  ];
 
-  
-  
-  // Listen to messages from content scripts if needed.
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    
+
+  chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: [1]}, () => {
+    chrome.declarativeNetRequest.updateDynamicRules({
+      addRules: conditions,
+    });
   });
+});
+
+
+
+chrome.webNavigation.onBeforeNavigate.addListener((details) => {
   
+  const url = details.url;
+  const serverEndpoint = "http://127.0.0.1:8000/urlapp/check/";
+  const urlMsg = { url };
+
+
+  fetch( serverEndpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(urlMsg),
+  })
+  .then((response) => {
+    if(!response.ok){
+      throw new Error("Network connection not OK")
+    }
+    return response.json();
+  })
+  .then((result) => {
+    if(result.is_malicious){
+      const tabId = details.tabId;
+      chrome.scripting.executeScript({
+        target: { tabId },
+        function: Warning,
+      })
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+})
+
+
+
+function Warning(){
+  const popUp = document.createElement("div");
+  popUp.id = 'warning';
+  popUp.innerText = 'This URL is suspected to be malicious of a potential XSS vulnerability, Proceed with caution';
+
+  document.body.appendChild(popUp);
+
+  setTimeout(() => {
+      popUp.remove();
+  }, 10000)
+
+}
   
