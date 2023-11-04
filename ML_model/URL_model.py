80408306273e920acc04b5cc10aa5ee68c0b4797
Ashade Samson <ashadesamson99@gmail.com>
2023-11-04 17:43:11 +0100

    added graph plots to the ML model

--- ML_model/URL_model.py
+++ ML_model/URL_model.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import pickle
-import re
 from urllib.parse import urlparse
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 import nltk
 from nltk.tokenize import word_tokenize
@@ -57,9 +62,49 @@ clf.fit(X_train, y_train)
 # Make predictions on the test set
 y_pred = clf.predict(X_test)
 
-# Evaluate accuracy
+# Evaluate Accuracy
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
 
 model_file = 'url_predict_model.pkl'
 pickle.dump(clf, open(model_file, 'wb'))
+
+
+
+# Precision
+precision = precision_score(y_test, y_pred, pos_label='defacement')
+print("Precision:", precision)
+
+
+
+# F1-Score
+f1 = f1_score(y_test, y_pred, pos_label='defacement')
+print("F1-Score:", f1)
+
+
+
+
+
+# Chart for accuracy, f1-score, and Precision
+labels = ['Accuracy', 'F1-Score', 'Precision']
+
+values = [accuracy, f1, precision]
+
+plt.bar(labels, values, color=['blue', 'green', 'red'])
+plt.ylabel('Score')
+plt.title('Accuracy, F1-Score, and Precision')
+plt.show()
+
+
+
+
+# Confusion Matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Confusion Matrix Plot
+plt.figure(figsize=(8, 6))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=True, yticklabels=True)
+plt.xlabel('Predicted URLs')
+plt.ylabel('True URLs')
+plt.title('Confusion Matrix')
+plt.show()
